1e998271ca0a5ce68da1c3840851adf6c6207538
freepath <devnull@localhost>
2017-09-08 15:33:02 +0200

    batch con supporto setup environment esterno

--- org.adempiere.server-feature/idempiere-server.bat
+++ org.adempiere.server-feature/idempiere-server.bat
@@ -1,5 +1,10 @@
 @Echo off
 
+set "BASE=%cd%"
+if exist "%BASE%\idempiere-env.bat" (
+	echo Reading env from idempiere-env.bat
+	call "%BASE%\idempiere-env.bat"
+)
 
 @if not "%JAVA_HOME%" == "" goto JAVA_HOME_OK
 @Set JAVA=java
@@ -13,9 +18,12 @@ goto START
 
 
 :START
+
 @Echo =======================================
 @Echo Starting iDempiere Server ...
+@echo "Using JAVA: %JAVA%"
+@echo "Using IDEMPIERE_OPTS: %IDEMPIERE_OPTS%"
 @Echo =======================================
 
 FOR %%c in (plugins\org.eclipse.equinox.launcher_1.*.jar) DO set JARFILE=%%c
-@"%JAVA%" -Dosgi.console=localhost:12612 -Djetty.home=jettyhome -Djetty.etc.config.urls=etc/jetty.xml,etc/jetty-selector.xml,etc/jetty-ssl.xml,etc/jetty-https.xml,etc/jetty-deployer.xml -XX:MaxPermSize=192m -Dmail.mime.encodefilename=true -Dmail.mime.decodefilename=true -Dmail.mime.encodeparameters=true -Dmail.mime.decodeparameters=true -jar %JARFILE% -application org.adempiere.server.application
+@"%JAVA%" %DEBUG% %IDEMPIERE_OPTS% -Dosgi.console=localhost:12612 -Djetty.home=jettyhome -Djetty.etc.config.urls=etc/jetty.xml,etc/jetty-selector.xml,etc/jetty-ssl.xml,etc/jetty-https.xml,etc/jetty-deployer.xml -XX:MaxPermSize=192m -Dmail.mime.encodefilename=true -Dmail.mime.decodefilename=true -Dmail.mime.encodeparameters=true -Dmail.mime.decodeparameters=true -jar %JARFILE% -application org.adempiere.server.application
--- org.adempiere.server-feature/idempiere-server.sh
+++ org.adempiere.server-feature/idempiere-server.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 #
+
+BASE=`dirname $( readlink -f idempiere-server.sh )`
+
+if [ -r "$BASE/idempiere-env.sh" ]; then
+  echo Reading env from idempiere-env.sh
+  . "$BASE/idempiere-env.sh"
+fi
+
 if [ $JAVA_HOME ]; then
   JAVA=$JAVA_HOME/bin/java
 else
@@ -13,10 +21,12 @@ if [ "$1" = "debug" ]; then
   DEBUG="-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=4554,server=y,suspend=n"
 fi
 
+unset DISPLAY
+
 echo ===================================
 echo Starting iDempiere Server
+echo "Using JAVA: $JAVA"
+echo "Using IDEMPIERE_OPTS: $IDEMPIERE_OPTS"
 echo ===================================
 
-unset DISPLAY
-BASE=`dirname $( readlink -f idempiere-server.sh )`
-$JAVA ${DEBUG} -Xbootclasspath/p:plugins/org.mortbay.jetty.alpn.boot_8.1.7.v20160121.jar -Dorg.osgi.framework.bootdelegation=sun.security.ssl,org.eclipse.jetty.alpn -Dosgi.compatibility.bootdelegation=true -Djetty.home=$BASE/jettyhome -Djetty.etc.config.urls=etc/jetty.xml,etc/jetty-deployer.xml,etc/jetty-ssl.xml,etc/jetty-ssl-context.xml,etc/jetty-http.xml,etc/jetty-alpn.xml,etc/jetty-http2.xml,etc/jetty-https.xml -Dosgi.console=localhost:12612 -Dmail.mime.encodefilename=true -Dmail.mime.decodefilename=true -Dmail.mime.encodeparameters=true -Dmail.mime.decodeparameters=true -jar $BASE/plugins/org.eclipse.equinox.launcher_1.*.jar -application org.adempiere.server.application
+$JAVA ${DEBUG} ${IDEMPIERE_OPTS} -Xbootclasspath/p:plugins/org.mortbay.jetty.alpn.boot_8.1.7.v20160121.jar -Dorg.osgi.framework.bootdelegation=sun.security.ssl,org.eclipse.jetty.alpn -Dosgi.compatibility.bootdelegation=true -Djetty.home=$BASE/jettyhome -Djetty.etc.config.urls=etc/jetty.xml,etc/jetty-deployer.xml,etc/jetty-ssl.xml,etc/jetty-ssl-context.xml,etc/jetty-http.xml,etc/jetty-alpn.xml,etc/jetty-http2.xml,etc/jetty-https.xml -Dosgi.console=localhost:12612 -Dmail.mime.encodefilename=true -Dmail.mime.decodefilename=true -Dmail.mime.encodeparameters=true -Dmail.mime.decodeparameters=true -jar $BASE/plugins/org.eclipse.equinox.launcher_1.*.jar -application org.adempiere.server.application
