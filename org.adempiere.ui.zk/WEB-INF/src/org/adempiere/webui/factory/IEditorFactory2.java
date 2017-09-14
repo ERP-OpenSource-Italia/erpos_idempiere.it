21cc7b2cc289e3b102b738b7bde000f63466d3b0
freepath <devnull@localhost>
2017-09-14 11:21:43 +0200

    IEditorFactory2

---
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/factory/IEditorFactory2.java
@@ -0,0 +1,26 @@
+package org.adempiere.webui.factory;
+
+import java.util.Properties;
+
+import org.adempiere.webui.editor.WEditor;
+import org.compiere.model.GridField;
+import org.compiere.model.GridTab;
+
+/**
+ * @author strinchero, www.freepath: manage editor for info window
+ *
+ */
+public interface IEditorFactory2 extends IEditorFactory
+{
+	/**
+	 * @param gridTab
+	 * @param gridField
+	 * @param tableEditor
+	 * @param infoEditor	editor is used by info window
+	 * @param WindowNo	WindiowNo
+	 * @param ctx	context (mandatory for editor for info window)
+	 * @return WEditor
+	 */
+	public WEditor getEditor(GridTab gridTab, GridField gridField, boolean tableEditor, boolean infoEditor, int WindowNo, Properties ctx);
+
+}
