1a558df025181199942e6f5b499febe041a4e356
Silvano Trinchero <strinchero@freepath.it>
2018-01-22 16:36:03 +0100

    (contribuilbile ?) Interfaccia customizzazione pay print

---
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/apps/form/IWPayPrint.java
@@ -0,0 +1,10 @@
+package org.adempiere.webui.apps.form;
+
+public interface IWPayPrint
+{
+	/**
+	 * 	Set Payment Selection
+	 *	@param C_PaySelection_ID id
+	 */
+	public void setPaySelection (int C_PaySelection_ID);
+}
