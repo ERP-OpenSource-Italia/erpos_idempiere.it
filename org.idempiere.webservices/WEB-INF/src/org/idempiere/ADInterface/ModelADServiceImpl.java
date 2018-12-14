4ca480114f66cfd698cc963cdfa09950beefaa3d
Silvano Trinchero <strinchero@freepath.it>
2018-12-14 17:15:39 +0100

    Porting da adempiere strip caratteri non validi per l'xml

--- org.idempiere.webservices/WEB-INF/src/org/idempiere/ADInterface/ModelADServiceImpl.java
+++ org.idempiere.webservices/WEB-INF/src/org/idempiere/ADInterface/ModelADServiceImpl.java
@@ -1513,7 +1513,16 @@ public class ModelADServiceImpl extends AbstractService implements ModelADServic
 							if(rsquery.getObject(columnName) instanceof byte[])
 								dfid.setVal(new String(Base64.encodeBase64(rsquery.getBytes(columnName))));						
 							else
-								dfid.setVal(rsquery.getString(columnName));
+							{
+								// F3P: strip non-valid xml chars
+								
+								String value = rsquery.getString(columnName);
+								
+								if(value != null)
+									value = stripNonValidXMLCharacters(value);
+
+								dfid.setVal(value);
+							}
 						}
 			    	}
 				}
@@ -1543,4 +1552,33 @@ public class ModelADServiceImpl extends AbstractService implements ModelADServic
 			getCompiereService().disconnect();
 		}
 	}
+	
+	/**
+	   * This method ensures that the output String has only
+	   * valid XML unicode characters as specified by the
+	   * XML 1.0 standard. For reference, please see
+	   * <a href="http://www.w3.org/TR/2000/REC-xml-20001006#NT-Char">the
+	   * standard</a>. This method will return an empty
+	   * String if the input is null or empty.
+	   *
+	   * @param in The String whose non-valid characters we want to remove.
+	   * @return The in String, stripped of non-valid characters.
+	   */
+	  public static String stripNonValidXMLCharacters(String in) {
+	      StringBuilder out = new StringBuilder(); // Used to hold the output.
+	      char current; // Used to reference the current character.
+
+	      if (in == null || ("".equals(in))) return ""; // vacancy test.
+	      for (int i = 0; i < in.length(); i++) {
+	          current = in.charAt(i); // NOTE: No IndexOutOfBoundsException caught here; it should not happen.
+	          if ((current == 0x9) ||
+	              (current == 0xA) ||
+	              (current == 0xD) ||
+	              ((current >= 0x20) && (current <= 0xD7FF)) ||
+	              ((current >= 0xE000) && (current <= 0xFFFD)) ||
+	              ((current >= 0x10000) && (current <= 0x10FFFF)))
+	              out.append(current);
+	      }
+	      return out.toString();
+	  }  	
 }
