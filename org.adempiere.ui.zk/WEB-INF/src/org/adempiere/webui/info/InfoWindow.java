cb8bde284ff75e7bd6fe3db67e5663013ec02527
Silvano Trinchero <strinchero@freepath.it>
2018-12-19 11:57:04 +0100

    Context for process execution

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/info/InfoWindow.java
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/info/InfoWindow.java
@@ -164,6 +164,7 @@ public class InfoWindow extends InfoPanel implements ValueChangeListener, EventL
 	private Map<Integer, List<Object>> cacheOriginalValues = new HashMap<>();
 	private Map<Integer, List<Object>> temporarySelectedData = new HashMap<>(); 	
 	private WInfoWindowListItemRenderer infoWindowListItemRenderer = null;
+	private int lastClickedMainContentRow = -1;
 	
 	// F3P: export 
 	
@@ -231,6 +232,7 @@ public class InfoWindow extends InfoPanel implements ValueChangeListener, EventL
    					}
    				}
    				   				
+   				lastClickedMainContentRow = row; // F3P: Keep track of last clicked row
    				updateSubcontent(row);
    			}
    		}); //xolali --end-
@@ -1739,7 +1741,8 @@ public class InfoWindow extends InfoPanel implements ValueChangeListener, EventL
 	@Override
     protected void executeQuery() {
     	prepareTable();    	
-    	cacheOriginalValues.clear(); // F3P: Clear original values     	
+    	cacheOriginalValues.clear(); // F3P: Clear original values   
+		lastClickedMainContentRow = -1; // F3P: clear last clicked row
     	super.executeQuery();
     }
     
@@ -2358,12 +2361,31 @@ public class InfoWindow extends InfoPanel implements ValueChangeListener, EventL
 	
 	// Edit Callback method and original values management
 	
-	public Properties getRowaAsCtx(int row, int editingColumn, Object editingValue)
+	/** Get row and params ctx. IF row is < 0, only params are used
+	 * 
+	 * @param row current row, ignored if < 0
+	 * @param editingColumn
+	 * @param editingValue
+	 * @return
+	 */
+	public Properties getRowAndParamsAsCtx(int row, int editingColumn, Object editingValue)
 	{
-		ListModelTable model = contentPanel.getModel();
 		Properties ctx = new Properties(Env.getCtx()); // Allow session values
-		
-		// Parameter dditors
+		return getRowAndParamsAsCtx(row, editingColumn, editingValue, 0, ctx);
+	}
+	
+	/** Get row and params ctx. IF row is < 0, only params are used
+	 * 
+	 * @param row
+	 * @param editingColumn
+	 * @param editingValue
+	 * @param WindowNo
+	 * @param ctx
+	 * @return
+	 */
+	public Properties getRowAndParamsAsCtx(int row, int editingColumn, Object editingValue, int WindowNo, Properties ctx)
+	{
+		// Parameter editors
 		
 		for(WEditor e:editors)
 		{
@@ -2373,19 +2395,52 @@ public class InfoWindow extends InfoPanel implements ValueChangeListener, EventL
 			if(val != null)
 			{
 				if(val instanceof Integer)
-					Env.setContext(ctx, 0, column, (Integer)val);
+					Env.setContext(ctx, WindowNo, column, (Integer)val);
 				else if(val instanceof Timestamp)
-					Env.setContext(ctx, 0, column, (Timestamp)val);
+					Env.setContext(ctx, WindowNo, column, (Timestamp)val);
 				else if(val instanceof Boolean)
-					Env.setContext(ctx, 0, column, (Boolean)val);
+					Env.setContext(ctx, WindowNo, column, (Boolean)val);
 				else
-					Env.setContext(ctx, 0, column, val.toString());
+					Env.setContext(ctx, WindowNo, column, val.toString());
+			}
+			else
+			{
+				Env.setContext(ctx, WindowNo, column, (String)null);
 			}
 		}
 		
-		for(int i=0; i < p_layout.length; i++)			
+		if (row >= 0)
+			getTableRowAsCtx(contentPanel, row, editingColumn, editingValue, WindowNo, ctx);
+
+		return ctx;
+	}
+	
+	public void resetTableCtx(WListbox table, int WindowNo, Properties ctx)
+	{
+		ColumnInfo[] layout = table.getLayout(); 
+		
+		for(int i=0; i < layout.length; i++)			
 		{			
-			String column = p_layout[i].getColumnName();
+			String column = layout[i].getColumnName();
+			
+			if(column == null)
+				column = layout[i].getGridField().getColumnName();
+			
+			Env.setContext(ctx, WindowNo, column, (String)null);
+		}
+	}
+	
+	public Properties getTableRowAsCtx(WListbox table, int row, int editingColumn, Object editingValue, int WindowNo, Properties ctx)
+	{
+		ListModelTable model = table.getModel();
+		ColumnInfo[] layout = table.getLayout(); 
+		
+		for(int i=0; i < layout.length; i++)			
+		{			
+			String column = layout[i].getColumnName();
+			
+			if(column == null)
+				column = layout[i].getGridField().getColumnName();
 					
 			Object val = null;
 			
@@ -2410,17 +2465,21 @@ public class InfoWindow extends InfoPanel implements ValueChangeListener, EventL
 				}
 								
 				if(val instanceof Integer)
-					Env.setContext(ctx, 0, column, (Integer)val);
+					Env.setContext(ctx, WindowNo, column, (Integer)val);
 				else if(val instanceof Timestamp)
-					Env.setContext(ctx, 0, column, (Timestamp)val);
+					Env.setContext(ctx, WindowNo, column, (Timestamp)val);
 				else if(val instanceof Boolean)
-					Env.setContext(ctx, 0, column, (Boolean)val);
+					Env.setContext(ctx, WindowNo, column, (Boolean)val);
 				else
-					Env.setContext(ctx, 0, column, val.toString());
-			}				
+					Env.setContext(ctx, WindowNo, column, val.toString());
+			}
+			else
+			{
+				Env.setContext(ctx, WindowNo, column, (String)null);
+			}
 		}
 		
-		return ctx;
+		return ctx;		
 	}
 	
 	public void onCellEditCallback(ValueChangeEvent event, int rowIndex, int colIndex, WEditor editor, GridField field)
@@ -2444,7 +2503,7 @@ public class InfoWindow extends InfoPanel implements ValueChangeListener, EventL
 		{
 			changeIsValid = false;
 			
-			Properties ctx = getRowaAsCtx(rowIndex, colIndex, val);
+			Properties ctx = getRowAndParamsAsCtx(rowIndex, colIndex, val);
 			
 			String rawSQL = infoColumn.getInputFieldValidation(); 
 			validationSQL = Env.parseContext(ctx, 0, rawSQL, false);
@@ -2580,7 +2639,6 @@ public class InfoWindow extends InfoPanel implements ValueChangeListener, EventL
 		
 		enableExportButton();
 	}
-		
 	
 	@Override
 	protected void updateListSelected()
@@ -2689,6 +2747,85 @@ public class InfoWindow extends InfoPanel implements ValueChangeListener, EventL
 		
 		exportButton.setEnabled(contentPanel.getRowCount() > 0);		
 	}
+
+	// F3P: fill context before running process 
+	
+	/** Get full ctx from selected rows, including parameters, selected main row, selected row of active embedded tab.
+	 *   This is the order applied, in case of conflicting columns with different values, they are overridden by later ones:
+	 *   
+	 *   1. parameters
+	 *   2. main content row (for mutliselection, the last clicked, if selected, is used. If its not selected, the first selected is used)
+	 *   3. selected embedded row
+	 *
+	 * @param WindowNo
+	 * @param ctx
+	 * @return
+	 */
+	public Properties getFullCtxFromSelectedRows(int WindowNo, Properties ctx)
+	{
+		// 0. Reset embedded tables context. (need to be done before main table is considered)
+		
+		for(int i=0; i < embeddedWinList.size(); i++)
+		{
+			EmbedWinInfo embedWinInfo = embeddedWinList.get(i);
+			WListbox listbox = (WListbox)embedWinInfo.getInfoTbl();
+			resetTableCtx(listbox, p_WindowNo, Env.getCtx());
+		}
+		
+		// 1 & 2: Parameters and selected active row
+		
+		int selectedRow = -1;
+		
+		// *** Context from main content		
+		// Check if last clicked is selected
+		
+		for(int sel:contentPanel.getSelectedIndices())
+		{
+			if(sel == lastClickedMainContentRow)
+			{
+				selectedRow = lastClickedMainContentRow;
+				break;
+			}
+		}
+		
+		if(selectedRow < 0)
+			selectedRow = contentPanel.getSelectedIndex();
+						
+		if(selectedRow >= 0)			
+			getRowAndParamsAsCtx(selectedRow, -1, null, p_WindowNo, Env.getCtx());
+		else
+		{
+			resetTableCtx(contentPanel, p_WindowNo, Env.getCtx());
+			getRowAndParamsAsCtx(-1, -1, null, p_WindowNo, Env.getCtx()); // Only parameters
+		}
+		
+		// 3: embedded tab
+		
+		if(embeddedPane.getTabs() != null &&
+				embeddedPane.getSelectedTab() != null)
+		{
+			int selectedIdx = embeddedPane.getSelectedIndex();
+			EmbedWinInfo embedWinInfo = embeddedWinList.get(selectedIdx);
+			WListbox listbox = (WListbox)embedWinInfo.getInfoTbl();
+			int embedSelectedRow = listbox.getSelectedRow();
+
+			if( embedSelectedRow >= 0)
+			{
+				getTableRowAsCtx(listbox, embedSelectedRow, -1, null, p_WindowNo, Env.getCtx());
+			}
+		}
+	
+		return ctx;
+	}
+	
+	// F3P: overriden to fill context for process
+	
+	@Override
+	protected void runProcess(Object processIdObj) 
+	{
+		getFullCtxFromSelectedRows(p_WindowNo, Env.getCtx());
+		super.runProcess(processIdObj);
+	}	
 	
 	private class XlsExportAction implements EventListener<Event>
 	{		
@@ -2854,5 +2991,5 @@ public class InfoWindow extends InfoPanel implements ValueChangeListener, EventL
 		{
 			return false;
 		}		
-	}	
+	}
 }
