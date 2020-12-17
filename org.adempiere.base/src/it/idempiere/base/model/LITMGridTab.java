f39bd123e712cbc3c2c0d4f2872bcc751a9516f5
Giulio Macchieraldo <macchieraldo@SISTEMI.LOCAL>
2020-12-17 16:47:48 +0100

    Fix callout al copia: i campi nascosti non hanno il gridTab = null

--- org.adempiere.base/src/it/idempiere/base/model/LITMGridTab.java
+++ org.adempiere.base/src/it/idempiere/base/model/LITMGridTab.java
@@ -10,7 +10,9 @@ public class LITMGridTab
 
 	public static boolean isLS_OverwriteCallout(GridTab gridTab) 
 	{
-		String LS_OverwriteCallout = DB.getSQLValueString(null, "SELECT LS_OverwriteCallout FROM AD_Tab WHERE AD_Tab_ID = ?",gridTab.getAD_Tab_ID());
+		String LS_OverwriteCallout = null;
+		if(gridTab != null)//I campi nascosti hanno non hanno il gridTab
+			LS_OverwriteCallout = DB.getSQLValueString(null, "SELECT LS_OverwriteCallout FROM AD_Tab WHERE AD_Tab_ID = ?",gridTab.getAD_Tab_ID());
 		
 		if(LS_OverwriteCallout != null 
 				&& LS_OverwriteCallout.equals("Y"))
