747880f2f32ce4ff47b9b3749ffdd45043ee8cb2
EnricoS <enrico.schio@labinfsistemi.it>
2022-04-06 09:13:55 +0200

    fix lancio timing import BP

--- org.adempiere.base.process/src/org/compiere/process/ImportBPartner.java
+++ org.adempiere.base.process/src/org/compiere/process/ImportBPartner.java
@@ -401,12 +401,13 @@ implements ImportProcess
 					if (impBP.getC_BPartner_ID() == 0)	//	Insert new BPartner
 					{
 						bp = new MBPartner(impBP);
-						ModelValidationEngine.get().fireImportValidate(this, impBP, bp, ImportValidator.TIMING_AFTER_IMPORT);
 						
 						setTypeOfBPartner(impBP,bp);
 						
+						ModelValidationEngine.get().fireImportValidate(this, impBP, bp, ImportValidator.TIMING_BEFORE_IMPORT);
 						if (SavedFromImport.save(bp))
 						{
+							ModelValidationEngine.get().fireImportValidate(this, impBP, bp, ImportValidator.TIMING_AFTER_IMPORT);
 							impBP.setC_BPartner_ID(bp.getC_BPartner_ID());
 							msglog = new StringBuilder("Insert BPartner - ").append(bp.getC_BPartner_ID());
 							if (log.isLoggable(Level.FINEST)) log.finest(msglog.toString());
@@ -443,13 +444,14 @@ implements ImportProcess
 							bp.setDescription(impBP.getDescription());
 						if (impBP.getC_BP_Group_ID() != 0)
 							bp.setC_BP_Group_ID(impBP.getC_BP_Group_ID());
-						ModelValidationEngine.get().fireImportValidate(this, impBP, bp, ImportValidator.TIMING_AFTER_IMPORT);
 						
 						setTypeOfBPartner(impBP,bp);
 						
 						//
+						ModelValidationEngine.get().fireImportValidate(this, impBP, bp, ImportValidator.TIMING_BEFORE_IMPORT);
 						if (SavedFromImport.save(bp))
 						{
+							ModelValidationEngine.get().fireImportValidate(this, impBP, bp, ImportValidator.TIMING_AFTER_IMPORT);
 							msglog = new StringBuilder("Update BPartner - ").append(bp.getC_BPartner_ID());
 							if (log.isLoggable(Level.FINEST)) log.finest(msglog.toString());
 							noUpdate++;
@@ -489,8 +491,9 @@ implements ImportProcess
 							bpl.setPhone2(impBP.getPhone2());
 						if (impBP.getFax() != null)
 							bpl.setFax(impBP.getFax());
-						ModelValidationEngine.get().fireImportValidate(this, impBP, bpl, ImportValidator.TIMING_AFTER_IMPORT);
+						ModelValidationEngine.get().fireImportValidate(this, impBP, bpl, ImportValidator.TIMING_BEFORE_IMPORT);
 						SavedFromImport.save(bpl);
+						ModelValidationEngine.get().fireImportValidate(this, impBP, bpl, ImportValidator.TIMING_AFTER_IMPORT);
 					}
 					//	New Location
 					else if (impBP.getC_Country_ID() != 0
@@ -524,9 +527,10 @@ implements ImportProcess
 						bpl.setPhone(impBP.getPhone());
 						bpl.setPhone2(impBP.getPhone2());
 						bpl.setFax(impBP.getFax());
-						ModelValidationEngine.get().fireImportValidate(this, impBP, bpl, ImportValidator.TIMING_AFTER_IMPORT);
+						ModelValidationEngine.get().fireImportValidate(this, impBP, bpl, ImportValidator.TIMING_BEFORE_IMPORT);
 						if (SavedFromImport.save(bpl))
 						{
+							ModelValidationEngine.get().fireImportValidate(this, impBP, bpl, ImportValidator.TIMING_AFTER_IMPORT);
 							msglog = new StringBuilder("Insert BP Location - ").append(bpl.getC_BPartner_Location_ID());
 							if (log.isLoggable(Level.FINEST)) log.finest(msglog.toString());
 							impBP.setC_BPartner_Location_ID(bpl.getC_BPartner_Location_ID());
@@ -589,9 +593,10 @@ implements ImportProcess
 						user.setBirthday(impBP.getBirthday());
 					if (bpl != null)
 						user.setC_BPartner_Location_ID(bpl.getC_BPartner_Location_ID());
-					ModelValidationEngine.get().fireImportValidate(this, impBP, user, ImportValidator.TIMING_AFTER_IMPORT);
+					ModelValidationEngine.get().fireImportValidate(this, impBP, user, ImportValidator.TIMING_BEFORE_IMPORT);
 					if (SavedFromImport.save(user))
 					{
+						ModelValidationEngine.get().fireImportValidate(this, impBP, user, ImportValidator.TIMING_AFTER_IMPORT);
 						msglog = new StringBuilder("Update BP Contact - ").append(user.getAD_User_ID());
 						if (log.isLoggable(Level.FINEST)) log.finest(msglog.toString());
 					}
@@ -627,9 +632,10 @@ implements ImportProcess
 						user.setBirthday(impBP.getBirthday());
 						if (bpl != null)
 							user.setC_BPartner_Location_ID(bpl.getC_BPartner_Location_ID());
-						ModelValidationEngine.get().fireImportValidate(this, impBP, user, ImportValidator.TIMING_AFTER_IMPORT);
+						ModelValidationEngine.get().fireImportValidate(this, impBP, user, ImportValidator.TIMING_BEFORE_IMPORT);
 						if (SavedFromImport.save(user))
 						{
+							ModelValidationEngine.get().fireImportValidate(this, impBP, user, ImportValidator.TIMING_AFTER_IMPORT);
 							msglog = new StringBuilder("Insert BP Contact - ").append(user.getAD_User_ID());
 							if (log.isLoggable(Level.FINEST)) log.finest(msglog.toString());
 							impBP.setAD_User_ID(user.getAD_User_ID());
@@ -653,7 +659,9 @@ implements ImportProcess
 					MContactInterest ci = MContactInterest.get(getCtx(), 
 							impBP.getR_InterestArea_ID(), user.getAD_User_ID(), 
 							true, get_TrxName());
+					ModelValidationEngine.get().fireImportValidate(this, impBP, ci, ImportValidator.TIMING_BEFORE_IMPORT);
 					SavedFromImport.save(ci);//	don't subscribe or re-activate
+					ModelValidationEngine.get().fireImportValidate(this, impBP, ci, ImportValidator.TIMING_AFTER_IMPORT);
 				}
 				
 				try
